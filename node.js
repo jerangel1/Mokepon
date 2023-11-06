7a4bcec3640c8db899c8a187103ab78b171a8ae6
Jesus Rangel <121629986+jerangel1@users.noreply.github.com>
2023-11-06 00:18:43 -0400

    app.post,class jugador y jugadorIndex

--- node.js
+++ node.js
@@ -1,11 +1,55 @@
 const express = require("express")
+const cors = require("cors")
 
 const app = express()
 
-app.get("/", (req, res) => {
-    res.send("hola")
+app.use(cors())
+app.use(express.json())
+
+const jugadores = []
+
+class Jugador {
+  constructor(id) {
+    this.id = id
+  }
+
+  asignarMokepon(mokepon) {
+    this.mokepon = mokepon
+  }
+}
+
+class Mokepon {
+  constructor(nombre) {
+    this.nombre = nombre
+  }
+}
+
+app.get("/unirse", (req, res) => {
+  const id = `${Math.random()}`
+
+  const jugador = new Jugador(id)
+
+  jugadores.push(jugador)
+
+  res.setHeader("Access-Control-Allow-Origin", "*")
+
+  res.send(id)
 })
 
+app.post("/mokepon/:jugadorId", (req, res) => {
+  const jugadorId = req.params.jugadorId || ""
+  const nombre = req.body.mokepon || ""
+  const mokepon = new Mokepon(nombre)
+
+  const jugadorIndex = jugadores.findIndex((jugador) => jugadorId === jugador.id)
+
+  if (jugadorIndex >= 0) {
+    jugadores[jugadorIndex].asignarMokepon(mokepon)
+  }
+  console.log(jugadores)
+  console.log(jugadorId)
+  res.end()
+})
 app.listen(8080, () => {
-    console.log("Servidor Funcionando")
+  console.log("Servidor funcionando")
 })
